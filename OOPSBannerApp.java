1f0e025e855b155477cddae517e3f230ffcf2a2c
sumedha-25 <sumedhamehta25@gmail.com>
2026-02-23 08:20:38 +0530

    UC2: Render OOPS banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,26 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author sumedha-25
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("  *****   " + "  *****   " + " *******  " + " *******  ");
+
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *      * ");
+
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *        ");
+
+        System.out.println(" *     *  " + " *     *  " + " *******  " + " *******  ");
+
+        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
+
+        System.out.println(" *     *  " + " *     *  " + " *        " + " *      * ");
+
+        System.out.println("  *****   " + "  *****   " + " *        " + " *******  ");
     }
 }
