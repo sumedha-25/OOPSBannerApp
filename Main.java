efa80af644e618884d79b10ed4bd39f3eb3e3cca
sumedha-25 <sumedhamehta25@gmail.com>
2026-02-23 09:12:36 +0530

    UC3 - Refactored banner using String.join() for improved memory efficiency

---
+++ Main.java
@@ -0,0 +1,34 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author sumedha-25
+ * @version 2.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " ******** ", "  ******** ", "  ******** ", "  ******** "));
+
+        System.out.println(String.join("",
+                " *      * ", "  *      * ", "  *      * ", "  *       "));
+
+        System.out.println(String.join("",
+                " *      * ", "  *      * ", "  *      * ", "  *       "));
+
+        System.out.println(String.join("",
+                " *      * ", "  *      * ", "  ******** ", "  ******** "));
+
+        System.out.println(String.join("",
+                " *      * ", "  *      * ", "  *        ", "         * "));
+
+        System.out.println(String.join("",
+                " *      * ", "  *      * ", "  *        ", "         * "));
+
+        System.out.println(String.join("",
+                " ******** ", "  ******** ", "  *        ", "  ******** "));
+    }
+}
