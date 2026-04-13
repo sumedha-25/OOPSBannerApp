b3fa9161247217f2b3001cac21fa604bf5fd3190
sumedha-25 <sumedhamehta25@gmail.com>
2026-04-13 10:20:54 +0530

    UC6 - Refactored banner using helper methods for each character pattern

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,58 @@
-/* 
- *
- * @author sumedha-25
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", "*******", "*******", "*******", "*******"),
-            String.join(" ", "*     *", "*     *", "*     *", "*"),
-            String.join(" ", "*     *", "*     *", "*     *", "*"),
-            String.join(" ", "*     *", "*     *", "*******", "*******"),
-            String.join(" ", "*     *", "*     *", "*", "      *"),
-            String.join(" ", "*     *", "*     *", "*", "      *"),
-            String.join(" ", "*******", "*******", "*", "*******")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // O Pattern
+    public static String[] getOPattern() {
+        return new String[]{
+                "*******",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*******"
+        };
+    }
+
+    // P Pattern
+    public static String[] getPPattern() {
+        return new String[]{
+                "*******",
+                "*     *",
+                "*     *",
+                "*******",
+                "*",
+                "*",
+                "*"
+        };
+    }
+
+    // S Pattern
+    public static String[] getSPattern() {
+        return new String[]{
+                "*******",
+                "*",
+                "*",
+                "*******",
+                "      *",
+                "      *",
+                "*******"
+        };
     }
 }
