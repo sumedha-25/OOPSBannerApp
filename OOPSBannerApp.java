9d70c53d90fd7645dac213a9977f8c82dcad3d33
sumedha-25 <sumedhamehta25@gmail.com>
2026-04-13 10:16:59 +0530

    feature/UC5: Inline array initialization using String.join()

---
+++ OOPSBannerApp.java
@@ -0,0 +1,24 @@
+/* 
+ *
+ * @author sumedha-25
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join(" ", "*******", "*******", "*******", "*******"),
+            String.join(" ", "*     *", "*     *", "*     *", "*"),
+            String.join(" ", "*     *", "*     *", "*     *", "*"),
+            String.join(" ", "*     *", "*     *", "*******", "*******"),
+            String.join(" ", "*     *", "*     *", "*", "      *"),
+            String.join(" ", "*     *", "*     *", "*", "      *"),
+            String.join(" ", "*******", "*******", "*", "*******")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
